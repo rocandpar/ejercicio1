de0cf89b00f55dff99025af31050d01494eb8b1d
rocandpar <randujarpareja@gmail.com>
2024-02-06 13:04:35 +0100

    Update ejercicio1.R

--- ejercicio1.R
+++ ejercicio1.R
@@ -6,8 +6,9 @@ n_registros <- 20
   tipo_artefacto <- sample(c("brazalete","ceramica","collar"), n_registros, replace = TRUE)
   cantidad_de_artefactos <- sample(1:150, n_registros, replace = TRUE )
   
-arcaheological_data <- data.frame(
-  yacimiento = yacimiento
-  tipo_artefacto = tipo_artefacto
+archaeological_data <- data.frame(
+  yacimiento = yacimiento,
+  tipo_artefacto = tipo_artefacto,
   cantidad_de_artefactos = cantidad_de_artefactos
 )
+print(archaeological_data)
